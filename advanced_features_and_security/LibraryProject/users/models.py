355278495f1361ac500ae483e309aa674920577d
Aderinola <aderinolaanna@gmail.com>
2025-12-03 06:40:09 +0100

    Add updates

--- advanced_features_and_security/LibraryProject/users/models.py
+++ advanced_features_and_security/LibraryProject/users/models.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    def __str__(self):
-        return self.username
+class CustomUser(AbstractBaseUser):
+    email =
+models.EmailField(unique=True)
+    phone_number =
+models.CharField(max_length=20)
+    # ... additional fields and methods as required ...
