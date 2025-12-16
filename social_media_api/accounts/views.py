6229225dadc199d213e8a7f5d8ac364508486089
Aderinola <aderinolaanna@gmail.com>
2025-12-16 11:05:50 +0100

    Add socialApi

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,51 +1,31 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .models import CustomUser
-from .serializers import UserRegistrationSerializer, UserLoginSerializer, CustomUserSerializer, UserProfileUpdateSerializer
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer, LoginSerializer
 
 
-# Create your views here.
-class UserRegistrationView(APIView):
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        serializer = UserRegistrationSerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "user": UserSerializer(user).data,
+                    "token": user.auth_token.key
+                },
+                status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-class UserLoginView(APIView):
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
-        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-    
-class UserProfileView(APIView):
-    def get(self, request, username):
-        try:
-            user = CustomUser.objects.get(username=username)
-            serializer = CustomUserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except CustomUser.DoesNotExist:
-            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-    def put(self, request, username):
-        try:
-            user = CustomUser.objects.get(username=username)
-            serializer = UserProfileUpdateSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except CustomUser.DoesNotExist:
-            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-# Create your views here.
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
